4bbc1e159577f0f4b99b5383d7e4d69462cedcb7
AlexandrMykhnovetski <674403324@ukr.net>
2022-06-03 12:52:05 +0300

    Update 1 03.06.2022

---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+const Profile = () => {
+return (
+    <div className='content'>
+    <div>
+      <img src='https://ychef.files.bbci.co.uk/976x549/p05xhy8l.jpg'/>
+    </div>       
+   <div>
+    ava+description
+  </div>
+  <div>
+    My posts
+      <div>
+        New post  
+      </div> 
+      <div>
+          <div>
+          Post1
+          </div>
+          <div>
+          Post2
+         </div>
+      </div>
+  </div>
+ </div>
+);
+}
+
+export default Profile;
